48f6c0981f5b15164e9b4ed9d3a5463069b20c32
EC2 Default User <ec2-user@ip-172-31-79-36.ec2.internal>
2022-04-11 20:58:39 +0000

    Rest JSON can now be native or JM

--- kubernetes/quarkus/rest-json-quickstart/startup.sh
+++ kubernetes/quarkus/rest-json-quickstart/startup.sh
@@ -1,7 +1,7 @@
 #/bin/bash
-if [ $NATIVE -eq 1 ]; then
-	./application -Dquarkus.http.host=0.0.0.0
+if [ -z "$NATIVE" ]; then
+	java -jar application.jar -Dquarkus.http.host=0.0.0.0
 else
-	java -jar rest-json-quickstart-1.0.0-SNAPSHOT.jar -Dquarkus.http.host=0.0.0.0
+	./application -Dquarkus.http.host=0.0.0.0
 fi
 
