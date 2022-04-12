8c23352d06def4d4b5b30b06eda3bdebbca9aeb2
EC2 Default User <ec2-user@ip-172-31-79-36.ec2.internal>
2022-04-12 16:50:16 +0000

    startup adjustment

--- kubernetes/quarkus/rest-json-quickstart/startup.sh
+++ kubernetes/quarkus/rest-json-quickstart/startup.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 if [ -z "$NATIVE" ]; then
-	java -jar application.jar -Dquarkus.http.host=0.0.0.0
-else
 	./application -Dquarkus.http.host=0.0.0.0
+else
+	java -jar application.jar -Dquarkus.http.host=0.0.0.0
 fi
 
